d1c2b8d24086ca4d77d98d5f928644cf5315acfd
Robert Joost <rjoost@influxdata.com>
2025-02-25 02:19:55 -0700

    major updates..will get better with atomic commits..

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -71,31 +71,57 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    -- if you already have an opts table, just add/merge the relevant sections
     opts = {
       servers = {
         lua_ls = {
           settings = {
             Lua = {
               diagnostics = {
-                globals = { "vim" },
+                globals = { "vim" }, -- Recognize 'vim' as a global variable
               },
+              workspace = {
+                library = function()
+                  return vim.api.nvim_get_runtime_file("", true) -- Now runs when the setting is accessed
+                end,
+                checkThirdParty = false,
+              },
+              telemetry = {
+                enable = false, -- Disable telemetry data collection
+              },
+            },
+          },
+        },
+        yamlls = { -- YAML Language Server
+          cmd = { "yaml-language-server", "--stdio" },
+          filetypes = { "yaml", "yml" },
+          settings = {
+            yaml = {
+              schemas = {
+                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+                ["https://json.schemastore.org/github-action.json"] = "/action.yml",
+                ["https://json.schemastore.org/kubernetes.json"] = "/*.k8s.yaml",
+              },
+              validate = true,
+              hover = true,
+              completion = true,
             },
           },
         },
       },
     },
   },
+
   -- Override jay-babu/mason-nvim-dap.nvim
   -- to fix issue with expand path
   -- Add DAP configuration
   {
-  "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require("dap")
+    "mfussenegger/nvim-dap",
+    config = function()
+      local vim = vim
+      local dap = require("dap")
     
-    -- Use the same Python as set in python3_host_prog
-    local python_path = vim.g.python3_host_prog
+      -- Use the same Python as set in python3_host_prog
+      local python_path = vim.g.python3_host_prog
     
     dap.adapters.python = {
       type = 'executable',
@@ -122,11 +148,7 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
     opts = {
       ensure_installed = {
-        "python",
-      },
-      handlers = {
-        python = function(source_name)
-        end,
+        "python", -- I already have debugpy but this keeps it in Mason
       },
     },
   },
@@ -147,12 +169,12 @@ return {
         "terraformls",
         "yamlls",           -- YAML (useful for Kubernetes, Ansible)
         "ansiblels",        -- Ansible specific
-        "azure_pipelines_ls", -- Azure Pipeline/GitHub Actions YAML
         
         -- Web/Config formats
         "jsonls",           -- JSON
         "lua_ls",           -- Lua
         "taplo",           -- TOML files
+        "marksman",          -- Markdown
         
         -- Cloud vendor specific
         -- "aws_cloudformation_yaml_ls", -- AWS CloudFormation
@@ -185,6 +207,7 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
+      local vim = vim
       require("monokai-pro").setup {
         filter = "pro",
         devicons = true,
@@ -220,7 +243,7 @@ return {
   { "preservim/nerdtree" },
 
   -- Status Bar
-  { "vim-airline/vim-airline" },
+  { "vim-airline/vim-airline", enabled = false },
 
   -- Fuzzy Finder
   { "ctrlpvim/ctrlp.vim" },
@@ -243,6 +266,39 @@ return {
   -- JQ Syntax Highlighting
   { "vito-c/jq.vim", ft = { "jq" } },
 
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional icon support
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "auto", -- or "tokyonight", "gruvbox", "onedark", etc.
+          icons_enabled = true,
+          component_separators = { left = "│", right = "│" },
+          section_separators = { left = "┤", right = "├" },
+          -- ... any other lualine options ...
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { { "filename", path = 1 } }, -- path=1 => show relative path
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        -- You can configure the "inactive" sections or extensions as well:
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+      })
+    end,
+  },
+
   -- Interactive JQ Buffers (optional)
   {
     "jrop/jq.nvim",
@@ -251,8 +307,18 @@ return {
   },
 
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    lazy = false, -- <- forces immediate load
+  },
+
+  { "norcalli/nvim-colorizer.lua" },
+  { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
+      local vim = vim
       local gs = require "gitsigns"
       gs.setup {
         -- Signs configuration
@@ -328,7 +394,7 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" }, -- Dependency
     config = function()
-      local null_ls = require "null-ls"
+      local null_ls = require("null-ls")
       null_ls.setup {
         -- To automatically format your Python, YAML, JSON, etc., on save.
         on_attach = function(client, bufnr)
@@ -374,6 +440,25 @@ return {
               "--prose-wrap=always"
             },
           }),
+          -- ADD THIS FOR PHP:
+          null_ls.builtins.formatting.prettier.with({
+            name = "prettier_php",
+            command = "prettier",
+            filetypes = { "php" },
+            args = { "--stdin-filepath", "$FILENAME", "--parser", "php" },
+          }),
+
+          -- open a .md file and do :lua vim.lsp.buf.format()
+          null_ls.builtins.formatting.prettier.with({
+            name = "prettier_markdown",
+            command = "prettier",
+            filetypes = { "markdown" },
+            args = { "--stdin-filepath", "$FILENAME", "--parser", "markdown" },
+          }),
+
+          null_ls.builtins.formatting.prettier.with({
+            filetypes = { "yaml", "yml" },
+          }),
 
           -- Diagnostics
           -- null_ls.builtins.formatting.yamlfmt, -- stricter than ansible-lint
@@ -381,11 +466,34 @@ return {
           null_ls.builtins.diagnostics.jsonlint,
           null_ls.builtins.diagnostics.cfn_lint, -- for CloudFormation/AWS
 
+          null_ls.builtins.diagnostics.shellcheck,
+          null_ls.builtins.formatting.shfmt,
+
           -- Code Actions
           null_ls.builtins.code_actions.gitsigns
         },
       }
     end,
+  },
+  {
+    -- Mason integration for null-ls
+    "jay-babu/mason-null-ls.nvim",
+    config = function(_, opts)
+      require("mason-null-ls").setup(opts)
+    end,
+    opts = {
+      ensure_installed = {
+        -- Formatters/Linters you want:
+      "black",        -- For Python formatting
+      "shellcheck",   -- For bash/zsh lint
+      "shfmt",        -- For bash/zsh formatting
+      "prettier",     -- For JSON/YAML/Markdown formatting
+      "jq",           -- For JSON or JQ formatting
+      "yamlfmt",      -- For YAML formatting
+      "jsonlint",     -- For JSON lint
+      },
+      automatic_installation = true,
+    },
   },
 
   -- Treesitter for better syntax highlighting
@@ -393,6 +501,7 @@ return {
   ["nvim-treesitter"] = {
     build = ":TSUpdate",
     config = function()
+      local vim = vim
       -- Setup Treesitter folding
       vim.opt.foldmethod = "expr"
       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
@@ -457,14 +566,18 @@ return {
       }
     end,
   },
+  { "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {},
+  },
   {
-    "zbirenbaum/copilot.lua",
+    "zbirenbaum/copilot.lua", -- https://github.com/zbirenbaum/copilot.lua
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
       require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
+        suggestion = { enabled = true },
+        panel = { enabled = true },
       })
     end,
   },
@@ -474,5 +587,179 @@ return {
       require("copilot_cmp").setup()
     end,
     dependencies = { "zbirenbaum/copilot.lua" },
+  },
+  -- In your plugins/user.lua, or a separate file for nvim-cmp overrides:
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "zbirenbaum/copilot-cmp",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require "cmp"
+      -- Insert Copilot source at the top or wherever you want
+      table.insert(opts.sources, 1, { name = "copilot", group_index = 2 })
+  
+      return opts
+    end,
+  },
+
+  -- Add these to your return table in plugins/user.lua
+-- Place them before the final closing brace
+
+  -- Telescope - Fuzzy finder on steroids
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- You already have this
+      "nvim-tree/nvim-web-devicons", -- You already have this
+      -- Telescope extensions
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+      "nvim-telescope/telescope-project.nvim",
+    },
+    cmd = "Telescope",
+    event = "VeryLazy",
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      
+      telescope.setup {
+        defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
+          path_display = { "truncate" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+          file_sorter = require("telescope.sorters").get_fuzzy_file,
+          file_ignore_patterns = {
+            "node_modules",
+            ".git/",
+            "__pycache__/",
+            "venv/",
+            ".venv/",
+            "*.pyc",
+          },
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-c>"] = actions.close,
+              ["<Down>"] = actions.move_selection_next,
+              ["<Up>"] = actions.move_selection_previous,
+              ["<CR>"] = actions.select_default,
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+              ["<C-t>"] = actions.select_tab,
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
+              ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+              ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+            },
+            n = {
+              ["<esc>"] = actions.close,
+              ["<CR>"] = actions.select_default,
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+              ["<C-t>"] = actions.select_tab,
+              ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+              ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+              ["j"] = actions.move_selection_next,
+              ["k"] = actions.move_selection_previous,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+          live_grep = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+          file_browser = {
+            theme = "dropdown",
+            hijack_netrw = true,
+          },
+          project = {},
+        },
+      }
+
+      -- Load Telescope extensions
+      telescope.load_extension("fzf")
+      telescope.load_extension("file_browser")
+      telescope.load_extension("project")
+    end,
+  },
+
+  -- Python enhancement plugins (these complement your existing Python setup)
+  -- For automatic docstring generation
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("neogen").setup({
+        enabled = true,
+        languages = {
+          python = {
+            template = {
+              annotation_convention = "google_docstrings",
+            },
+          },
+        },
+      })
+    end,
+    cmd = "Neogen",
+    keys = {
+      { "<Leader>ds", "<cmd>Neogen func<CR>", desc = "Generate function docstring" },
+      { "<Leader>dc", "<cmd>Neogen class<CR>", desc = "Generate class docstring" },
+    },
+  },
+
+  -- For improved Python indentation (you have nvim-treesitter but this adds PEP8 specifics)
+  {
+    "Vimjas/vim-python-pep8-indent",
+    ft = "python",
+  },
+
+  -- For enhanced Python navigation beyond basic treesitter
+  {
+    "jeetsukumaran/vim-pythonsense",
+    ft = "python",
+  },
+
+  -- For better YAML support with focus on Ansible (complements your existing YAML support)
+  {
+    "cuducos/yaml.nvim",
+    ft = { "yaml", "yaml.ansible" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
   },
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -3,17 +3,50 @@
 -- fit in the normal config locations above can go here
 
 return function()
+  -- Make a local reference to the global vim variable
+  local vim = vim
   -- Global Variables
   vim.g.mapleader = " "
   vim.g.maplocalleader = " "
 
+  -- Configure diagnostics display for better error visualization
+  -- Add this near the beginning of your polish.lua function
+  vim.diagnostic.config({
+    virtual_text = {
+      prefix = '●', -- Could be '■', '▎', 'x', etc
+      spacing = 4,
+      source = "if_many",
+      severity = {
+        min = vim.diagnostic.severity.HINT,
+      },
+    },
+    float = {
+      source = true,
+      border = "rounded",
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+  })
+
+  -- Customize diagnostic signs
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
+
   -- Load essential plugins immediately
   local plugins_to_load = {
-    "jq.nvim",
-    "neo-tree.nvim",
-    "nvim-colorizer.lua",
-    "plenary.nvim",
-    "todo-comments.nvim"
+    "lewis6991/gitsigns.nvim",
+    "jrop/jq.nvim",
+    "folke/lazydev.nvim",        -- Only include if you've added it above
+    "nvim-neo-tree/neo-tree.nvim",
+    "norcalli/nvim-colorizer.lua",
+    "nvim-lua/plenary.nvim",
+    "folke/todo-comments.nvim",
+    "nvim-telescope/telescope.nvim",
   }
 
   for _, plugin in ipairs(plugins_to_load) do
@@ -25,10 +58,10 @@ return function()
   opt.clipboard = "unnamedplus"   -- Use system clipboard
   opt.number = false              -- No line numbers
   opt.relativenumber = false      -- No relative line numbers
-  opt.tabstop = 2                -- Number of spaces for tab
-  opt.shiftwidth = 2             -- Number of spaces for autoindent
-  opt.expandtab = true           -- Convert tabs to spaces
-  opt.termguicolors = true       -- True color support
+  opt.tabstop = 2                 -- Number of spaces for tab
+  opt.shiftwidth = 2              -- Number of spaces for autoindent
+  opt.expandtab = true            -- Convert tabs to spaces
+  opt.termguicolors = true        -- True color support
 
   -- Treesitter Configuration
   opt.runtimepath:append(vim.fn.stdpath "data" .. "/treesitter_parsers")
@@ -38,7 +71,7 @@ return function()
 
   -- Create autocommands
   local autocmd = vim.api.nvim_create_autocmd
-  
+
   -- Remove trailing whitespace on save
   autocmd("BufWritePre", {
     pattern = "*",
@@ -56,18 +89,44 @@ return function()
       vim.schedule(function()
         require("lazy").load({
           plugins = {
-            "gitsigns.nvim",
-            "jq.nvim",
-            "lazydev.nvim",
-            "neo-tree.nvim",
-            "nvim-colorizer.lua",
-            "plenary.nvim",
-            "todo-comments.nvim",
+            "lewis6991/gitsigns.nvim",
+            "jrop/jq.nvim",
+            "folke/lazydev.nvim",        -- Only include if you've added it above
+            "nvim-neo-tree/neo-tree.nvim",
+            "norcalli/nvim-colorizer.lua",
+            "nvim-lua/plenary.nvim",
+            "folke/todo-comments.nvim",
           }
         })
       end)
     end,
     desc = "Load essential plugins early",
+  })
+
+  -- YAML-specific settings (new additions)
+  autocmd("FileType", {
+    pattern = { "yaml", "yml", "yaml.ansible" },
+    callback = function()
+      local opts = { noremap = true, silent = true }
+      local keymap = vim.keymap.set
+
+      -- Set YAML-specific options for indentation and formatting
+      vim.opt_local.expandtab = true
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+      vim.opt_local.softtabstop = 2
+
+      -- Add ansible filetype detection for improved syntax highlighting (optional)
+      autocmd("BufRead,BufNewFile", {
+        pattern = { "*/playbooks/*.yml", "*/roles/*.yml", "*/inventory/*.yml" },
+        command = "set filetype=yaml.ansible",
+        desc = "Set Ansible YAML filetype for better syntax highlighting",
+      })
+
+      -- Keybinding: Format YAML file using LSP formatting
+      keymap("n", "<Leader>yf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+    end,
+    desc = "YAML file settings and keybindings",
   })
 
   -- JQ Configuration
@@ -76,9 +135,49 @@ return function()
   -- JQ Keybindings
   local keymap = vim.keymap.set
   keymap("n", "<Leader>fj", "<Cmd>%!jq<CR>", opts)                    -- Format entire buffer
-  keymap("n", "<Leader>fcj", "<Cmd>%!jq --compact-output<CR>", opts) -- Minify entire buffer
-  keymap("v", "<Leader>fj", ":'<,'>!jq<CR>", opts)                   -- Format selection
-  keymap("v", "<Leader>fcj", ":'<,'>!jq --compact-output<CR>", opts) -- Minify selection
+  keymap("n", "<Leader>fcj", "<Cmd>%!jq --compact-output<CR>", opts)  -- Minify entire buffer
+  keymap("v", "<Leader>fj", ":'<,'>!jq<CR>", opts)                    -- Format selection
+  keymap("v", "<Leader>fcj", ":'<,'>!jq --compact-output<CR>", opts)  -- Minify selection
+
+  -- Main telescope functions
+  keymap("n", "<Leader>tf", "<cmd>Telescope find_files<CR>", opts)    -- Find files
+  keymap("n", "<Leader>tg", "<cmd>Telescope live_grep<CR>", opts)     -- Find text in files
+  keymap("n", "<Leader>tb", "<cmd>Telescope buffers<CR>", opts)       -- Find open buffers
+  keymap("n", "<Leader>th", "<cmd>Telescope help_tags<CR>", opts)     -- Find help tags
+  keymap("n", "<Leader>tr", "<cmd>Telescope oldfiles<CR>", opts)      -- Find recent files
+  keymap("n", "<Leader>tc", "<cmd>Telescope colorscheme<CR>", opts)   -- Find colorschemes
+
+  -- Project management
+  keymap("n", "<Leader>tp", "<cmd>Telescope project<CR>", opts)       -- Find projects
+
+  -- File browser
+  keymap("n", "<Leader>te", "<cmd>Telescope file_browser<CR>", opts)  -- File explorer
+
+  -- LSP related
+  keymap("n", "<Leader>ts", "<cmd>Telescope lsp_document_symbols<CR>", opts)    -- Document symbols
+  keymap("n", "<Leader>tS", "<cmd>Telescope lsp_workspace_symbols<CR>", opts)   -- Workspace symbols
+  keymap("n", "<Leader>td", "<cmd>Telescope lsp_definitions<CR>", opts)         -- Go to definition
+  keymap("n", "<Leader>tt", "<cmd>Telescope lsp_type_definitions<CR>", opts)    -- Go to type definition
+  keymap("n", "<Leader>tr", "<cmd>Telescope lsp_references<CR>", opts)          -- Find references
+  keymap("n", "<Leader>ti", "<cmd>Telescope lsp_implementations<CR>", opts)     -- Find implementations
+
+  -- Git operations
+  keymap("n", "<Leader>gc", "<cmd>Telescope git_commits<CR>", opts)   -- Git commits
+  keymap("n", "<Leader>gb", "<cmd>Telescope git_branches<CR>", opts)  -- Git branches
+  keymap("n", "<Leader>gs", "<cmd>Telescope git_status<CR>", opts)    -- Git status
+
+  -- Custom Telescope functions
+  -- Find Python files only
+  keymap("n", "<Leader>py", "<cmd>Telescope find_files find_command=find,.,'-name','*.py'<CR>", opts)
+
+  -- Find Ansible YAML files
+  keymap("n", "<Leader>ya", "<cmd>Telescope find_files find_command=find,.,'-name','*.yml','-o','-name','*.yaml'<CR>", opts)
+
+  -- Find JSON files
+  keymap("n", "<Leader>js", "<cmd>Telescope find_files find_command=find,.,'-name','*.json'<CR>", opts)
+
+  -- Find TODOs
+  keymap("n", "<Leader>to", "<cmd>Telescope grep_string search='TODO\\|FIXME\\|BUG\\|HACK\\|NOTE'<CR>", opts)
 
   -- Automatically format JSON on save
   autocmd("BufWritePre", {
@@ -91,4 +190,71 @@ return function()
 
   -- LSP Format keybinding
   keymap("n", "<Leader>ff", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+
+  -- Create autocommands for Python files
+  autocmd("FileType", {
+    pattern = "python",
+    callback = function()
+      -- Set Python-specific options
+      vim.opt_local.expandtab = true
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.tabstop = 4
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.colorcolumn = "88"  -- For Black formatter line length
+
+      -- Python specific keymappings
+      keymap("n", "<F5>", "<cmd>lua require('dap').continue()<CR>", opts)
+      keymap("n", "<F10>", "<cmd>lua require('dap').step_over()<CR>", opts)
+      keymap("n", "<F11>", "<cmd>lua require('dap').step_into()<CR>", opts)
+      keymap("n", "<F12>", "<cmd>lua require('dap').step_out()<CR>", opts)
+      keymap("n", "<Leader>b", "<cmd>lua require('dap').toggle_breakpoint()<CR>", opts)
+      keymap("n", "<Leader>dr", "<cmd>lua require('dap').repl.open()<CR>", opts)
+
+      -- Auto-generate docstring shortcut
+      keymap("n", "<Leader>pd", "<cmd>Neogen func<CR>", opts)
+
+      -- Run current Python file
+      keymap("n", "<Leader>pr", "<cmd>!python %<CR>", opts)
+    end,
+    desc = "Python file settings",
+  })
+
+  -- Create autocommands for YAML files
+  autocmd("FileType", {
+    pattern = { "yaml", "yaml.ansible" },
+    callback = function()
+      -- Set YAML-specific options
+      vim.opt_local.expandtab = true
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+      vim.opt_local.softtabstop = 2
+
+      -- Add ansible filetype detection for improved syntax highlighting
+      autocmd("BufRead,BufNewFile", {
+        pattern = { "*/playbooks/*.yml", "*/roles/*.yml", "*/inventory/*.yml" },
+        command = "set filetype=yaml.ansible",
+      })
+
+      -- YAML formatting on save
+      keymap("n", "<Leader>yf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+    end,
+    desc = "YAML file settings",
+  })
+
+  -- JSON-specific settings
+  autocmd("FileType", {
+    pattern = "json",
+    callback = function()
+      -- Set JSON-specific options
+      vim.opt_local.expandtab = true
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+      vim.opt_local.softtabstop = 2
+
+      -- Additional JSON keymappings beyond what you already have
+      keymap("n", "<Leader>jp", "<cmd>%!python -m json.tool<CR>", opts)  -- Alternative JSON formatter
+    end,
+    desc = "JSON file settings",
+  })
 end
+
